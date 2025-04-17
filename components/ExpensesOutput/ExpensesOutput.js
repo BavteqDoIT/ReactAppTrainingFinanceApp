f514eef7152a8964a1d0926ee443d6aeb7c01d13
BavteqDoIT <bartek.adamek@onet.pl>
2025-04-17 17:43:06 +0200

    Initial commit

---
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -0,0 +1,56 @@
+import { StyleSheet, View } from "react-native";
+import ExpensesSummary from "./ExpensesSummary";
+import ExpensesList from "./ExpensesList";
+import { GlobalStyles } from "../../constants/styles";
+
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    description: "A pair of shoes",
+    amount: 59.99,
+    date: new Date("2025-04-16"),
+  },
+  {
+    id: "e2",
+    description: "A pair of trousers",
+    amount: 89.91,
+    date: new Date("2025-03-25"),
+  },
+  {
+    id: "e3",
+    description: "Some bananas",
+    amount: 5.5,
+    date: new Date("2025-04-17"),
+  },
+  {
+    id: "e4",
+    description: "A book",
+    amount: 14.87,
+    date: new Date("2025-03-16"),
+  },
+  {
+    id: "e5",
+    description: "Another book",
+    amount: 18.59,
+    date: new Date("2025-04-04"),
+  },
+];
+
+function ExpensesOutput({ expenses, expensesPeriod }) {
+  return (
+    <View style={styles.container}>
+      <ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod} />
+      <ExpensesList expenses={DUMMY_EXPENSES}/>
+    </View>
+  );
+}
+
+export default ExpensesOutput;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary700
+  }
+});
