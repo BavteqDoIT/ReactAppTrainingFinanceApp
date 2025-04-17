9fae66f6fa1fddbac5e29a28ae56d591d331effc
BavteqDoIT <bartek.adamek@onet.pl>
2025-04-17 18:05:01 +0200

    Lesson 134 - Improved list of expenses

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -34,6 +34,36 @@ const DUMMY_EXPENSES = [
     amount: 18.59,
     date: new Date("2025-04-04"),
   },
+  {
+    id: "e6",
+    description: "A pair of shoes",
+    amount: 59.99,
+    date: new Date("2025-04-16"),
+  },
+  {
+    id: "e7",
+    description: "A pair of trousers",
+    amount: 89.91,
+    date: new Date("2025-03-25"),
+  },
+  {
+    id: "e8",
+    description: "Some bananas",
+    amount: 5.5,
+    date: new Date("2025-04-17"),
+  },
+  {
+    id: "e9",
+    description: "A book",
+    amount: 14.87,
+    date: new Date("2025-03-16"),
+  },
+  {
+    id: "e0",
+    description: "Another book",
+    amount: 18.59,
+    date: new Date("2025-04-04"),
+  },
 ];
 
 function ExpensesOutput({ expenses, expensesPeriod }) {
@@ -50,7 +80,9 @@ export default ExpensesOutput;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 24,
+    paddingHorizontal: 24,
+    paddingTop: 24,
+    paddingBottom: 0,
     backgroundColor: GlobalStyles.colors.primary700
   }
 });
