8605c85b00bce433ba6eeb7363f6d61165047134
BavteqDoIT <bartek.adamek@onet.pl>
2025-04-18 16:51:36 +0200

    Lesson 143 - Working on adding/deleting/updating expenses tbc.

--- components/ExpensesOutput/ExpenseItem.js
+++ components/ExpensesOutput/ExpenseItem.js
@@ -1,16 +1,31 @@
 import { Pressable, StyleSheet, Text, View } from "react-native";
 import { GlobalStyles } from "../../constants/styles";
+import { getFormattedDate } from "../../util/date";
+import { useNavigation } from "@react-navigation/native";
 
-function ExpenseItem({ description, amount, date }) {
+function ExpenseItem({id, description, amount, date }) {
+  const navigation = useNavigation();
+
+  function expensePressHandler() {
+    navigation.navigate("ManageExpense", {
+      expenseId: id
+    });
+  }
+
   return (
-    <Pressable>
+    <Pressable
+      onPress={expensePressHandler}
+      style={({ pressed }) => pressed && styles.pressed}
+    >
       <View style={styles.expenseItem}>
         <View>
-          <Text style={[styles.textBase, styles.description]}>{description}</Text>
-          <Text style={styles.textBase}>{date.toString()}</Text>
+          <Text style={[styles.textBase, styles.description]}>
+            {description}
+          </Text>
+          <Text style={styles.textBase}>{getFormattedDate(date)}</Text>
         </View>
         <View style={styles.amountContainer}>
-          <Text style={styles.amount}>{amount}</Text>
+          <Text style={styles.amount}>{amount.toFixed(2)}</Text>
         </View>
       </View>
     </Pressable>
@@ -20,6 +35,9 @@ function ExpenseItem({ description, amount, date }) {
 export default ExpenseItem;
 
 const styles = StyleSheet.create({
+  pressed: {
+    opacity: 0.75,
+  },
   expenseItem: {
     padding: 12,
     marginVertical: 8,
@@ -34,12 +52,12 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.4,
   },
   textBase: {
-    color: GlobalStyles.colors.primary50
+    color: GlobalStyles.colors.primary50,
   },
   description: {
     fontSize: 16,
     marginBottom: 4,
-    fontWeight: "bold"
+    fontWeight: "bold",
   },
   amountContainer: {
     paddingHorizontal: 12,
@@ -48,9 +66,10 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     borderRadius: 4,
+    minWidth: 80,
   },
   amount: {
     color: GlobalStyles.colors.primary500,
-    fontWeight: "bold"
-  }
+    fontWeight: "bold",
+  },
 });
--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -3,68 +3,7 @@ import ExpensesSummary from "./ExpensesSummary";
 import ExpensesList from "./ExpensesList";
 import { GlobalStyles } from "../../constants/styles";
 
-const DUMMY_EXPENSES = [
-  {
-    id: "e1",
-    description: "A pair of shoes",
-    amount: 59.99,
-    date: new Date("2025-04-16"),
-  },
-  {
-    id: "e2",
-    description: "A pair of trousers",
-    amount: 89.91,
-    date: new Date("2025-03-25"),
-  },
-  {
-    id: "e3",
-    description: "Some bananas",
-    amount: 5.5,
-    date: new Date("2025-04-17"),
-  },
-  {
-    id: "e4",
-    description: "A book",
-    amount: 14.87,
-    date: new Date("2025-03-16"),
-  },
-  {
-    id: "e5",
-    description: "Another book",
-    amount: 18.59,
-    date: new Date("2025-04-04"),
-  },
-  {
-    id: "e6",
-    description: "A pair of shoes",
-    amount: 59.99,
-    date: new Date("2025-04-16"),
-  },
-  {
-    id: "e7",
-    description: "A pair of trousers",
-    amount: 89.91,
-    date: new Date("2025-03-25"),
-  },
-  {
-    id: "e8",
-    description: "Some bananas",
-    amount: 5.5,
-    date: new Date("2025-04-17"),
-  },
-  {
-    id: "e9",
-    description: "A book",
-    amount: 14.87,
-    date: new Date("2025-03-16"),
-  },
-  {
-    id: "e0",
-    description: "Another book",
-    amount: 18.59,
-    date: new Date("2025-04-04"),
-  },
-];
+
 
 function ExpensesOutput({ expenses, expensesPeriod }) {
   return (
