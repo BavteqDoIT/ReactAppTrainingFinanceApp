801de60d59b9c78ec9fa4ad7d657bded4e0df3df
BavteqDoIT <bartek.adamek@onet.pl>
2025-04-29 22:03:45 +0200

    Lesson 198 - Added camera permissions on IOS

--- components/Places/ImagePicker.js
+++ components/Places/ImagePicker.js
@@ -1,24 +1,52 @@
-import { Button, View } from "react-native";
-import {launchCameraAsync} from "expo-image-picker"
+import { Alert, Button, View } from "react-native";
+import {
+  launchCameraAsync,
+  useCameraPermissions,
+  PermissionStatus,
+} from "expo-image-picker";
 
-function ImagePicker(){
-    async function takeImageHandler(){
-        const image = await launchCameraAsync({
-            allowsEditing: true,
-            aspect: [16, 9],
-            quality: 0.5,
-        });
-        console.log(image);
+function ImagePicker() {
+  const [cameraPermissionInformation, requestPermission] =
+    useCameraPermissions();
+
+  async function verifyPermissions() {
+    if (cameraPermissionInformation.status === PermissionStatus.UNDETERMINED) {
+      const permissionResponse = await requestPermission();
+
+      return permissionResponse.granted;
     }
 
-    return(
-        <View>
-            <View>
+    if (cameraPermissionInformation.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        "Insufficient Permissions!",
+        "You need to grant camera permissions to use this app."
+      );
+      return false;
+    }
 
-            </View>
-            <Button title="Take Image" onPress={takeImageHandler} />
-        </View>
-    );
+    return true;
+  }
+
+  async function takeImageHandler() {
+    const hasPermission = await verifyPermissions();
+    
+    if(!hasPermission){
+        return;
+    }
+    const image = await launchCameraAsync({
+      allowsEditing: true,
+      aspect: [16, 9],
+      quality: 0.5,
+    });
+    console.log(image);
+  }
+
+  return (
+    <View>
+      <View></View>
+      <Button title="Take Image" onPress={takeImageHandler} />
+    </View>
+  );
 }
 
 export default ImagePicker;
